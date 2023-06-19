28fc44c85199980c0460e0e3d1a01a0a7b41c4c2
rozelie <ryan.ozelie@gmail.com>
2023-06-19 12:12:58 -0500

    Fix season file loading to reload data if all team data is not present

--- baseball_obp_and_cobp/retrosheet.py
+++ baseball_obp_and_cobp/retrosheet.py
@@ -10,16 +10,16 @@ from baseball_obp_and_cobp import paths
 RETROSHEET_URL = "https://www.retrosheet.org"
 
 
-def get_seasons_event_files(year: int) -> Iterator[Path]:
+def get_seasons_event_files(year: int) -> list[Path]:
     data_year_dir = paths.DATA_DIR / str(year)
-    seasons_events_file = _get_seasons_events_file(data_year_dir, year)
-    if seasons_events_file:
+    seasons_events_file = list(_get_seasons_events_file(data_year_dir, year))
+    if seasons_events_file and len(seasons_events_file) == 30:
         return seasons_events_file
 
     seasons_event_files_zip_content = _get_years_event_files_zip_content(year)
     data_year_dir.mkdir(parents=True, exist_ok=True)
     seasons_event_files_zip_content.extractall(data_year_dir.as_posix())
-    return _get_seasons_events_file(data_year_dir, year)
+    return list(_get_seasons_events_file(data_year_dir, year))
 
 
 def _get_years_event_files_zip_content(year: int) -> ZipFile:
