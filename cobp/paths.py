fe47b51c7032a04fe259f2afd18d94c8e2cc83da
rozelie <ryan.ozelie@gmail.com>
2024-02-05 13:58:45 -0600

    Move local dir to user's home dir

--- cobp/paths.py
+++ cobp/paths.py
@@ -1,7 +1,12 @@
 """Local project path constants."""
 from pathlib import Path
 
-DATA_DIR = Path("/tmp/retrosheet")
-DISCREPANCY_DIR = Path("/tmp/retrosheet/discrepancies")
+COBP_DIR = Path.home() / ".cobp"
+DATA_DIR = COBP_DIR / "data"
+DISCREPANCY_DIR = COBP_DIR / "discrepancies"
+
+for dir in [COBP_DIR, DATA_DIR, DISCREPANCY_DIR]:
+    dir.mkdir(exist_ok=True, parents=True)
+
 PROJECT_ROOT = Path(__file__).parents[1]
 DOTENV = PROJECT_ROOT / ".env"
