9eef224006e14054de4b817b82f85131f95f399d
rozelie <ryan.ozelie@gmail.com>
2024-02-14 12:46:28 -0600

    Remove old discrepancy code

--- cobp/paths.py
+++ cobp/paths.py
@@ -3,9 +3,8 @@ from pathlib import Path
 
 COBP_DIR = Path.home() / ".cobp"
 DATA_DIR = COBP_DIR / "data"
-DISCREPANCY_DIR = COBP_DIR / "discrepancies"
 
-for dir in [COBP_DIR, DATA_DIR, DISCREPANCY_DIR]:
+for dir in [COBP_DIR, DATA_DIR]:
     dir.mkdir(exist_ok=True, parents=True)
 
 PROJECT_ROOT = Path(__file__).parents[1]
